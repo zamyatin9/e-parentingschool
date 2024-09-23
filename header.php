e4e2a28302c4a08f4fdfcf71e4bd5af0fd4fc9a5
zamyatin9 <ap.zamyatin@gmail.com>
2024-09-23 09:48:43 +0500

    v

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/header.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="element_header">
+            <div class="element_menu">
+                <div class="horizontal_list">
+                    <p><a href="#">Главная</a></p>
+                    <p><a href="#">О нас</a></p>
+                    <p><a href="#">Отзывы</a></p>
+                </div>
+            </div>
+            <div class="btm_go_to_course">
+                <a href="#">Перейти к курсу</a>
+            </div>
+        </div>
+    </div>
