1b52381c2a7c0ef227d13bc91ddf6c4a6b9838a9
zamyatin9 <ap.zamyatin@gmail.com>
2024-09-23 19:14:44 +0500

    23/09/2024

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 
     <?php include 'header.php'?>
+    <?php include 'main.php'?>
 </body>
 </html>
---
+++ main.php
@@ -0,0 +1,15 @@
+<div class="hero_img">
+    <div class="text_header">
+        <p id="h1">Школа современных родителей</p>
+        <p>онлайн курс</p>
+    </div>
+     <div class="container_btms_hero_img">
+        <div class="btm_contact">
+            <p>Связаться с нами</p>
+            </div>
+        <div class="btm_more_detailed">
+            <p>Подробнее</p>
+        </div>
+     </div>    
+    <!-- <img src="img/hero.png"> -->
+</div>
