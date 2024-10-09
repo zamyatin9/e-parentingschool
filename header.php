774e6269ac087f3ae2aaf96cce093266945d4188
zamyatin9 <ap.zamyatin@gmail.com>
2024-10-09 12:31:18 +0500

    Update header.php

--- header.php
+++ header.php
@@ -13,8 +13,22 @@
         <link rel="stylesheet" href="style/resulte.css">
         <link rel="stylesheet" href="style/aftor.css">
         <link rel="stylesheet" href="style/footer.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.1/aos.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.1/aos.js"></script>
+       <!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(98532874, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+            });
+        </script>
+<noscript><div><img src="https://mc.yandex.ru/watch/98532874" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
     <title>Школа современных родителей</title>
 </head>
 <body>
